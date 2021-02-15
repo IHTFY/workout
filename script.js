f5311bc714f404fe71b4ffc84665bff3a5329a05
Frankie Mercado <oeluer@gmail.com>
2021-02-14 21:51:41 -0800

    fix -0 in beat the average

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function updateTotal(db) {
   const diff = averages[averages.length - 1] - dailyTotals[daysSince];
   document.getElementById('compareAverage').textContent = diff > 0
     ? `${Math.ceil(diff).toLocaleString()} to beat average`
-    : `Beat the average by ${Math.floor(-diff).toLocaleString()}!`;
+    : `Beat the average by ${Math.floor(Math.abs(diff)).toLocaleString()}!`;
 
 
   // Update editable dbText
